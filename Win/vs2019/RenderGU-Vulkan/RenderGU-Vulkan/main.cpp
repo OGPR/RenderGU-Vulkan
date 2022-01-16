c44b757307c89580c6f937bb91936665984c9d5b
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-01-16 18:49:50 +1030

    Test Vulkan Instance extensions and GLM

--- Win/vs2019/RenderGU-Vulkan/RenderGU-Vulkan/main.cpp
+++ Win/vs2019/RenderGU-Vulkan/RenderGU-Vulkan/main.cpp
@@ -1,4 +1,4 @@
-#define GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN // This includes vulkan 
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
@@ -7,6 +7,7 @@
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
+#include <assert.h>
 
 int main()
 {
@@ -15,11 +16,28 @@ int main()
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // NO_API so we set up glfw window for Vulkan
     auto window = glfwCreateWindow(800, 600, "RenderGU-Vulkan", nullptr, nullptr);
 
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    assert(extensionCount > 0);
+
+    std::cout << "Extension count: " << extensionCount << std::endl;
+
+    // Test GLM
+    glm::mat4 IMat(1.0f);
+    glm::vec4 Vec(1.0f);
+    auto result = IMat * Vec;
+    // End Test GLM
+        
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 
 }

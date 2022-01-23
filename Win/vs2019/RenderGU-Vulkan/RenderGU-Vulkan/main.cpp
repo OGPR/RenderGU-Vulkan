98a6c730e0f750ae602dd0b2be2b60cef72069a0
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2022-01-23 13:09:39 +1030

    Extracted window creation to class

--- Win/vs2019/RenderGU-Vulkan/RenderGU-Vulkan/main.cpp
+++ Win/vs2019/RenderGU-Vulkan/RenderGU-Vulkan/main.cpp
@@ -1,33 +1,10 @@
-#define GLFW_INCLUDE_VULKAN // This includes vulkan 
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
-#include <assert.h>
+#include "VulkanWindow.h"
 
+VulkanWindow vulkanWindow;
 int main()
 {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // NO_API so we set up glfw window for Vulkan
-    auto window = glfwCreateWindow(800, 600, "RenderGU-Vulkan", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    assert(extensionCount > 0);
-
-    std::cout << "Extension count: " << extensionCount << std::endl;
-
-    // Test GLM
-    glm::mat4 IMat(1.0f);
-    glm::vec4 Vec(1.0f);
-    auto result = IMat * Vec;
-    // End Test GLM
-        
+    GLFWwindow* window = vulkanWindow.init("RenderGU_Windows", 800, 600);
 
     while (!glfwWindowShouldClose(window))
     {
